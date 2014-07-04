93fb706ab8b6dabd7fb0109315b4767f5a9f04e1
alphonso64 <alphonso64@126.com>
2014-07-04 17:07:41 +0800

    CVS1.1
    add socket

--- CVS/CVS/gen/com/example/cvs/R.java
+++ CVS/CVS/gen/com/example/cvs/R.java
@@ -18,23 +18,43 @@ public final class R {
     
          */
         public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_textview_margin=0x7f040002;
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int backgroud=0x7f020000;
         public static final int bga=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int bgb=0x7f020002;
+        public static final int black=0x7f020009;
+        public static final int blue=0x7f02000a;
+        public static final int cyan=0x7f02000b;
+        public static final int darkgray=0x7f020007;
+        public static final int dkgray=0x7f020004;
+        public static final int gray=0x7f02000c;
+        public static final int green=0x7f02000d;
+        public static final int ic_launcher=0x7f020003;
+        public static final int lightgreen=0x7f020008;
+        public static final int ltgray=0x7f02000e;
+        public static final int magenta=0x7f02000f;
+        public static final int red=0x7f020010;
+        public static final int transparent=0x7f020011;
+        public static final int white=0x7f020006;
+        public static final int yello=0x7f020005;
+        public static final int yellow=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
         public static final int button1=0x7f080002;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080000;
-        public static final int textView1=0x7f080003;
+        public static final int listView=0x7f080003;
+        public static final int name=0x7f080004;
+        public static final int value=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int infocheck=0x7f030001;
+        public static final int item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- CVS/CVS/src/com/example/cvs/MainActivity.java
+++ CVS/CVS/src/com/example/cvs/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.cvs;
 
+
+import Utility.Packet;
+import Utility.socketClient;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -9,13 +15,18 @@ import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	socketClient soC;
+	Handler loginHandler;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,10 +35,57 @@ public class MainActivity extends Activity {
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN); // (NEW) 
-        
-        Button btn = (Button) findViewById(R.id.button1);
-       // btn.setAlpha(100);
 		setContentView(R.layout.activity_main);
+		
+		loginHandler = new Handler() {
+
+
+			@Override
+			public void handleMessage(Message msg) {
+				if(msg.what==0)
+				{
+					  Intent intent = new Intent();
+					  intent.setClass(MainActivity.this, InfoCheck.class);
+					  startActivity(intent);
+				}else
+				{
+					new  AlertDialog.Builder(MainActivity.this) .setMessage("Login Error" ).setPositiveButton("OK" ,  null ).show();
+				}
+				
+			}
+			
+		};
+		
+		  EditText edx1 = (EditText)findViewById(R.id.editText1);
+		  edx1.setOnFocusChangeListener(new OnFocusChangeListener() {
+			    public void onFocusChange(View v, boolean hasFocus) {
+			        EditText _v=(EditText)v;
+			        if (!hasFocus) {// 失去焦点
+			            _v.setHint(_v.getTag().toString());
+			        } else {
+			            String hint=_v.getHint().toString();
+			            _v.setTag(hint);
+			            _v.setHint("");
+			        }
+			    }
+			});
+		  EditText edx2 = (EditText)findViewById(R.id.editText2);
+		  edx2.setOnFocusChangeListener(new OnFocusChangeListener() {
+			    public void onFocusChange(View v, boolean hasFocus) {
+			        EditText _v=(EditText)v;
+			        if (!hasFocus) {// 失去焦点
+			            _v.setHint(_v.getTag().toString());
+			        } else {
+			            String hint=_v.getHint().toString();
+			            _v.setTag(hint);
+			            _v.setHint("");
+			        }
+			    }
+			});
+		
+		startSocket();
+		soC.setLoginHandler(loginHandler);
+		
 	}
 	
 	public void buttonOnClick(View view)
@@ -37,30 +95,70 @@ public class MainActivity extends Activity {
 	  
 	  Editable s1 = edx1.getText();
 	  Editable s2 = edx2.getText();
-	  String a = "admin";
-	  String b = "123456";
-//	  if( s1.toString().equals(a) && s2.toString().equals(b))
-//	  {
-//		  Log.e("aa","login success");
-//		  Intent intent = new Intent();
-//		  intent.setClass(MainActivity.this, InfoCheck.class);
-//		  startActivity(intent);
-//	  }else{
-//		  new  AlertDialog.Builder(this) .setMessage("Login Error" ).setPositiveButton("OK" ,  null ).show(); 
-//	  }
-		  
-	  Intent intent = new Intent();
-	  intent.setClass(MainActivity.this, InfoCheck.class);
-	  startActivity(intent);
-	  
+	  Packet pkg = new Packet(s1.toString(), s2.toString());
+	  soC.Send(pkg.getBuf());
+  
 	}
 	
+	public void startSocket() {
+		Log.e("life", "hit 11");
+		soC = socketClient.getInstance();		
+		//soC =new socketClient();
+		soC.start();
+	}
+
+	private void stopSocket() {
+		soC.isRun = false;
+		soC.close();
+		soC.destroyInstance();
+		soC = null;
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.e("life","onStart");
+	}
+
+	@Override
+	protected void onRestart() {
+		Log.e("life","onRestart");
+		//startSocket();
+		super.onRestart();
+	
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.e("life","onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.e("life","onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		//stopSocket();
+		Log.e("life","onStop");
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		stopSocket();
+		super.onDestroy();
+		Log.e("life","onDestroy");
 	}
 
 }
